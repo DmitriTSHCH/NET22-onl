014abd475ed30a0cb4115b473e523ed98c86fa50
DmitriTSHCH <tshchetko@mail.ru>
2025-11-19 23:57:20 +0300

    added option to continue operations with the result

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -10,28 +10,54 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Выберете операцию (+,-,/,*):");
             string oper = Console.ReadLine();
-            Console.WriteLine("Выберете число1:");
+            Console.WriteLine("Введите число1:");
             int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Выберете число2:");
+            Console.WriteLine("Введите число2:");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
-            switch (oper)
+            int result = 0;
+            string continueCheckInput;
+
+            do
             {
-                case "+":
-                    Console.WriteLine((num1 + num2));
+                switch (oper)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine("Результат = " + (result));
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine("Результат = " + (result));
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine("Результат = " + (result));
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine("Результат = " + (result));
+                        break;
+                }
+                Console.WriteLine("Желаете продолжить? (Y/N)");
+                continueCheckInput = Console.ReadLine();
+
+                if (continueCheckInput == "N")
+                {
                     break;
-                case "-":
-                    Console.WriteLine((num1 - num2));
-                    break;
-                case "*":
-                    Console.WriteLine((num1 * num2));
-                    break;
-                case "/":
-                    Console.WriteLine((num1 / num2));
-                    break;
+                }
+                num1 = result;
+                Console.WriteLine("Выберете операцию (+,-,/,*):");
+                oper = Console.ReadLine();
+                Console.WriteLine("Введите число:");
+                num2 = Convert.ToInt32(Console.ReadLine());
             }
+            while ( continueCheckInput == "Y" );
+
+
             Console.ReadKey();
         }
     }
