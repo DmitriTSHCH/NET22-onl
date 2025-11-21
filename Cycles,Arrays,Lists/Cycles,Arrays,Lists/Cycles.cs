f8eb1edbd6d8c4b7f9e92187cb2fd7cdcfef95b7
DmitriTSHCH <tshchetko@mail.ru>
2025-11-21 23:17:39 +0300

    create Cycles.cs

---
+++ Cycles,Arrays,Lists/Cycles,Arrays,Lists/Cycles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cycles_Arrays_Lists
+{
+    internal class Cycles
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число для начала перечисления до 1: ");
+
+            for (int i = Convert.ToInt32(Console.ReadLine()); i > 0; i--)
+            {
+                Console.Write($"{i},");
+            }
+            Console.Write("\n\nНажмите любую клавишу для продолжения");
+            //Console.ReadKey();
+            //Console.Write("");
+
+
+        }
+    }
+}
