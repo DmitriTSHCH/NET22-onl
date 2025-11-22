74822585a77177c86660333ce60aed7149db56a0
DmitriTSHCH <tshchetko@mail.ru>
2025-11-22 11:23:20 +0300

    Added sequence of multiples of 7 and Fibonacci

--- Cycles,Arrays,Lists/Cycles,Arrays,Lists/Cycles.cs
+++ Cycles,Arrays,Lists/Cycles,Arrays,Lists/Cycles.cs
@@ -16,11 +16,37 @@ namespace Cycles_Arrays_Lists
             {
                 Console.Write($"{i},");
             }
-            Console.Write("\n\nНажмите любую клавишу для продолжения");
-            //Console.ReadKey();
-            //Console.Write("");
+            
+            Console.Write("\b.\nНажмите любую клавишу для продолжения\n");
+            Console.ReadKey();
+            
+            Console.Write("\bВывод чисел кратных 7.\nВведите до какого числа вывести последовательность: ");
+            int limitNumber = Convert.ToInt32(Console.ReadLine());
 
+            for (int i = 1; (i*7) < limitNumber; i++)
+            {
+                Console.Write((i * 7) + ",");
+            }
 
+            Console.Write("\b.\nНажмите любую клавишу для продолжения\n");
+            Console.ReadKey();
+
+            Console.Write("\bВывод последовательности Фибоначчи.\nВведите до какого числа вывести последовательность: ");
+            limitNumber = Convert.ToInt32(Console.ReadLine());
+
+            int num1 = 0;
+            int num2 = 1;
+            Console.Write($"{num1},{num2},");
+
+            while (limitNumber > num2+num2)
+            {
+                int numN = num2;
+                num2 = num1 + num2;
+                num1 = numN;
+                Console.Write($"{num2},");
+            }
+
+            Console.Write("\b.\n");
         }
     }
 }
