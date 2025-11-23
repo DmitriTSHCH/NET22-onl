131ab56efb6cb487742e69b917dd4590e1b9bae3
DmitriTSHCH <tshchetko@mail.ru>
2025-11-23 15:03:54 +0300

    added counting positive & negative numbers and removing numbers from a string when the parity does not match

--- Arrays/Arrays/Arrays.cs
+++ Arrays/Arrays/Arrays.cs
@@ -10,50 +10,83 @@ namespace Cycles_Arrays_Lists
     {
         static void Main(string[] args)
         {
+            int[,] matrix = CreateArray();
+            matrix = RandomizeArray(matrix);
+            PrintArray(matrix);
+
             while (true)
             {
-                int[,] matrix = CreateArray();
-
-                matrix = RandomizeArray(matrix);
-
                 PrintArray(matrix);
-            }
-            /*
-            Console.WriteLine("(1);\n(2);\n(3);\nВыберите нужную последовательность(введите номер варианта): ");
-
-            while (true)
-            {
-                Console.SetCursorPosition(0, 8);
-
-                PrintArray(arr);
-
-                for (int i = 0; i < args.Length; i++)
-                {
-
-                }
-
-
-
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("(1)Создать новую матрицу;\n(2)Подсчитать количество положительных и отрицательных чисел;\n(3)Оставить только четные значения в четных строках и нечетные - в нечетных строках;\n(4)Подсчет количества чесел в матрице.\n\nВыберите действие(введите номер варианта): ");
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        //
+                        matrix = CreateArray();
+                        matrix = RandomizeArray(matrix);
                         break;
                     case "2":
-                        //
+                        int quantityPositiveNumber = 0;
+                        int quantityNegativeNumber = 0;
+                        foreach (int i in matrix)
+                        {
+                            if (i > 0)
+                            {
+                                quantityPositiveNumber++;
+                            }
+                            else if (i < 0)
+                            {
+                                quantityNegativeNumber++;
+                            }
+                        }
+                        PrintArray(matrix);
+                        Console.SetCursorPosition(0, 0);
+                        Console.WriteLine($"Количество положительных: {quantityPositiveNumber}\nКоличество отрицательных: {quantityNegativeNumber}\nКоличество нулей: { matrix.Length - (quantityPositiveNumber + quantityNegativeNumber) }\nНажмите любую клавишу для продолжения");
+                        Console.ReadKey();
                         break;
                     case "3":
-                        //
+                        for (int i = 0; i < matrix.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < matrix.GetLength(1); j++)
+                            {
+                                if ((matrix[i, j] % 2) == 0 && i % 2 != 0)
+                                {
+                                    matrix[i, j] = 0;
+                                }
+                                else if ((matrix[i, j] % 2) != 0 && i % 2 == 0)
+                                {
+                                    matrix[i, j] = 0;
+                                }
+                            }
+                        }
+                        PrintArrayWithoutZero(matrix);
+                        Console.SetCursorPosition(0, 0);
+                        Console.WriteLine($"Нажмите любую клавишу для продолжения");
+                        Console.ReadKey();
+                        break;
+                    case "4":
+                        //подсчет количества через слварь
                         break;
                 }
-            }*/
+            }
         }
-        static int[,] CreateArray ()
+        static int[,] CreateArray()
         {
-            Console.Write("Введите числол строк матрицы (не более 5): ");
-            int n = Convert.ToInt16(Console.ReadLine());
-            Console.Write("\nВведите числол столбцов матрицы (не более 5): ");
-            int m = Convert.ToInt16(Console.ReadLine());
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            int n, m;
+            do
+            {
+                Console.Write("Введите числол строк матрицы (не более 5): ");
+                n = Convert.ToInt16(Console.ReadLine());
+            }
+            while (n > 5 | n < 1);
+            do
+            {
+                Console.Write("\nВведите числол столбцов матрицы (не более 5): ");
+                m = Convert.ToInt16(Console.ReadLine());
+            }
+            while (m > 5 | m < 1);
             int[,] array = new int[n, m];
             return array;
         }
@@ -71,6 +104,8 @@ namespace Cycles_Arrays_Lists
         }
         static void PrintArray(int[,] array)
         {
+            Console.Clear();
+            Console.SetCursorPosition(0, 10);
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
@@ -80,6 +115,35 @@ namespace Cycles_Arrays_Lists
                         Console.Write(" ");
                     }
                     Console.Write($"{array[i, j]} ");
+                    if (j == (array.GetLength(1)) - 1)
+                    {
+                        Console.Write("\n");
+                    }
+                }
+            }
+        }
+        static void PrintArrayWithoutZero(int[,] array)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 10);
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > -1)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    if (array[i, j] == 0)
+                    {
+                        Console.Write($"* ");
+                    }
+                    else
+                    {
+                        Console.Write($"{array[i, j]} ");
+                    }
+
                     if (j == (array.GetLength(1)) - 1)
                     {
                         Console.Write("\n");
