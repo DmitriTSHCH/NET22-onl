a3d879ef6d1292f8803d83d45f7d16f220209f14
DmitriTSHCH <tshchetko@mail.ru>
2025-11-30 23:14:22 +0300

    create console app PlasticCard

---
+++ lesson7/PlasticCard/PlasticCard/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Chars_Strings
+{
+    internal class CharsAndStrings
+    {
+        static void Main(string[] args)
+        {
+            BankCard card1 = new BankCard();
+            BankCard card2 = new BankCard();
+            BankCard card3 = new BankCard();
+            while (true)
+            {
+                Console.WriteLine("Выберите одну из трех карт");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                        case 1:
+                        card1.SelectOperation();
+                        break;
+                        case 2:
+                        card2.SelectOperation();
+                        break;
+                        case 3:
+                        card3.SelectOperation();
+                        break;
+                }
+            }
+        }
+    }
+}
