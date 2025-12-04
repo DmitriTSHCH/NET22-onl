e61b5a634664f08910c97bbc22880e5d12401ad0
DmitriTSHCH <tshchetko@mail.ru>
2025-12-04 22:27:57 +0300

    create console app lesson9

---
+++ lesson9/lesson9/IVehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson9
+{
+    internal interface IVehicle
+    {
+        public void Drive(int driveLength);
+        public bool Refuel(int gasVolume);
+        
+
+    }
+}
---
+++ lesson9/lesson9/Truck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson9
+{
+    internal class Truck : Car
+    {
+        public Truck(int _gasTank) : base(_gasTank)
+        {
+            fuelСonsumption = 10;
+            Console.WriteLine($"Грузовик создан\nБензин: {gasTank}\nРасход: {fuelСonsumption}");
+        }
+    }
+}
