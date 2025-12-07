59185cd6b21fbbd73f37138f33b5735efd26f0fc
DmitriTSHCH <tshchetko@mail.ru>
2025-12-07 16:13:53 +0300

    create console app Vocation

---
+++ lesson8/Vocation/Vocation/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vocation
+{
+    internal class Worker : Employee
+    {
+        private string Name;
+        private string Vocation = "Рабочий";
+        public Worker(string _name)
+        {
+            Name = _name;
+        }
+        public override void SetName(string _name)
+        {
+            Name = _name;
+        }
+        public override string GetName()
+        {
+            return Name;
+        }
+        public override void PrintVocation()
+        {
+            Console.WriteLine($"Должность: {Vocation}");
+        }
+    }
+}
