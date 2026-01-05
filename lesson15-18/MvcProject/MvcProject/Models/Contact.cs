e302568b6335c889aecb5f81769949f4e5fa03d5
DmitriTSHCH <tshchetko@mail.ru>
2026-01-05 19:00:51 +0300

    create MVC project and add new page Contacts

---
+++ lesson15-18/MvcProject/MvcProject/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcProject.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Имя")]
+        [Required(ErrorMessage = "Нужно ввести имя")]
+        public string _name { get; set; }
+
+        [Display(Name = "Фамилия")]
+        [Required(ErrorMessage = "Нужно ввести фамилию")]
+        public string _surname { get; set; }
+
+        [Display(Name = "Электронная почта")]
+        [Required(ErrorMessage = "Нужно ввести адрес эл. почты")]
+        public string _email { get; set; }
+
+        [Display(Name = "Сообщение")]
+        [Required(ErrorMessage = "Нужно ввести сообщение")]
+        [StringLength(30, ErrorMessage = "Сообщение должно содержать не более 30 символов")]
+        public string _message { get; set; }
+    }
+}
