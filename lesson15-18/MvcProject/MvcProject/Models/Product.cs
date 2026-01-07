cbe822c8d8f5fb959798e1d8c85892a9b6d8fa96
DmitriTSHCH <tshchetko@mail.ru>
2026-01-07 21:01:55 +0300

    create models Product and Storage, create view Storage(not ready)

---
+++ lesson15-18/MvcProject/MvcProject/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace MvcProject.Models
+{
+    public enum Category { electronics, furniture, cloth, other, [EditorBrowsable(EditorBrowsableState.Never)] unspecified }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category? Category { get; set; }
+        public string? Description { get; set; }
+
+        public Product (int id, string name, Category? category, string? description)
+        {
+            Id = id;
+            Name = name;
+
+            if (category != null)
+            {
+                Category = category;
+            }
+            else
+            { 
+                Category = Models.Category.unspecified;
+            }
+
+            Description = description ?? "*отсутствует*";
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{Id}. Название: {Name}, Категория: {Category}, Описание: {Description}");
+        }
+    }
+}
