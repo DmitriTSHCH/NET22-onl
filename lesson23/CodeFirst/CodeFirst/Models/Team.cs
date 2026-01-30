c4cf02981d22688782c099527bad17eddfad2991
DmitriTSHCH <tshchetko@mail.ru>
2026-01-30 18:25:08 +0300

    creat app DbFirst and CodeFirst

---
+++ lesson23/CodeFirst/CodeFirst/Models/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeFirst.Models;
+
+public partial class Team
+{
+    public int TeamId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public int? TrainerId { get; set; }
+
+    public int? Rate { get; set; }
+
+    public virtual ICollection<Player> Players { get; set; } = new List<Player>();
+
+    public virtual Trainer? Trainer { get; set; }
+}
---
+++ lesson23/CodeFirst/CodeFirst/Models/Trainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeFirst.Models;
+
+public partial class Trainer
+{
+    public int TrainerId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Team> Teams { get; set; } = new List<Team>();
+}
